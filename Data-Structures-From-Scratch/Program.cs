3cfc46f4644111b9730063ade4e567d5d76f1e1f
Gulzar Dalal <gdalal2009@gmail.com>
2023-03-01 17:12:18 -0500

    Updated main Data Structures Project with the LinkedList implementation

--- Data-Structures-From-Scratch/Program.cs
+++ Data-Structures-From-Scratch/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Data_Structures_From_Scratch
+﻿using LinkedList;
+using System.Security.Cryptography;
+
+namespace Data_Structures_From_Scratch
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var rnd = new Random();
+            var list = new LinkedList.LinkedList<int>(new Node<int>(rnd.Next()), new Node<int>(rnd.Next()));
+
+            list.DisplayLinkedList();
             Console.WriteLine("Hello, World!");
         }
     }
